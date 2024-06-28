82d369ae32fe0b8949ebe08ad6bb57b839eab629
Nelson Mbau <mbau.ndungu@gmail.com>
2024-06-28 19:22:55 +0300

    feat: initialize database with prisma, import current models

---
+++ packages/database/.eslintrc.js
@@ -0,0 +1,8 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ['@stewardly/eslint-config/library.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+}
