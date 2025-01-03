8c68076f4a6b5840dd0b7de3210fa2b5eafa1b1d
Nelson Mbau <mbau.ndungu@gmail.com>
2025-01-03 13:08:46 +0300

    feat: add email/password auth

--- packages/auth/src/server/index.ts
+++ packages/auth/src/server/index.ts
@@ -1,11 +1,22 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+// import { discord } from 'better-auth/social-providers'
 import { db } from '@stewardly/database'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 10,
+  },
+  // socialProviders: {
+  //   discord: {
+  //     clientId: process.env.DISCORD_CLIENT_ID!,
+  //     clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+  //   },
+  // },
 })
 
 export type Session = typeof auth.$Infer.Session
